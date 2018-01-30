0ef18c0587e26aaa68c13dabbd32f9fd51d3d1fa
Gordon Mikkelsen <gordonmikkelsen87@gmail.com>
2018-01-30 14:52:36 +0100

    TestCase Car createt

---
+++ BilletLib/BilletLib/Bil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib
+{
+    public class Bil
+    {
+        private string _nummerplade;
+        private DateTime _dato;
+
+        public String NummerPlade { get { return _nummerplade; } }
+        
+        public DateTime Dato { get { return _dato; } }
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
---
+++ BilletLib/BilletLibTests/BilTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            // Arrange
+            Bil b1 = new Bil();
+
+            // Act
+
+            var minBil1 = b1.Pris();
+
+            // Asset
+            Assert.AreEqual(240, minBil1 );
+        }
+
+        [TestMethod()]
+        public void køretøjTest()
+        {
+            // Arrange
+            Bil b2 = new Bil();
+
+            // Act
+
+            var minBil2 = b2.køretøj();
+
+            //  Asset
+            Assert.AreEqual("Bil", minBil2);
+        }
+    }
+}
